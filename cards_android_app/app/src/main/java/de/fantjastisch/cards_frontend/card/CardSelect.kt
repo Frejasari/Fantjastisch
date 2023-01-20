b67728eddf6ce608f526ac64c37b63ff512e65d5
Semjon Nirmann <semnir@uni-bremen.de>
2023-01-20 14:39:31 +0100

    remove scroll vom card/Category select wrappers, introduce card select...

--- cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/card/CardSelect.kt
+++ cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/card/CardSelect.kt
@@ -1,21 +1,17 @@
 package de.fantjastisch.cards_frontend.card
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import java.util.*
 
 data class CardSelectItem(
     val id: UUID, val question: String, val answer: String, val tag: String,
-    val categories: List<UUID>, val isChecked: Boolean
+    val categories: List<String>, val isChecked: Boolean
 )
 
 @Composable
@@ -24,43 +20,14 @@ fun CardSelect(
     cards: List<CardSelectItem>,
     onCardSelected: (UUID) -> Unit = {}
 ) {
-    // Ein RecyclerView -> Eine lange liste von Eintraegen
-    LazyColumn(
+    Column(
         modifier = modifier
-            .fillMaxWidth(),
-        contentPadding = PaddingValues(vertical = 16.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
     ) {
-        items(cards) { card ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    modifier = Modifier
-                        .weight(1f),
-                    text = card.question
-                )
-                Text(
-                    modifier = Modifier
-                        .weight(1f),
-                    text = card.answer
-                )
-                Text(
-                    modifier = Modifier
-                        .weight(1f),
-                    text = card.tag
-                )
-                Text(
-                    modifier = Modifier
-                        .weight(1f),
-                    text = card.categories.toString()
-                )
-                Checkbox(
-                    checked = card.isChecked,
-                    onCheckedChange = { onCardSelected(card.id) }
-                )
-            }
+        cards.map { card ->
+            CardSelectComponent(card = card, onCardSelected = onCardSelected)
         }
     }
 }
---
+++ cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/card/CardSelectComponent.kt
@@ -0,0 +1,88 @@
+package de.fantjastisch.cards_frontend.card
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.*
+
+@Composable
+fun CardSelectComponent(
+    card: CardSelectItem,
+    onCardSelected: (UUID) -> Unit = {}
+) {
+    Card(
+        modifier = Modifier,
+        shape = CardDefaults.elevatedShape,
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.elevatedCardColors(
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            containerColor = MaterialTheme.colorScheme.background
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 16.dp),
+
+            ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier
+                        .weight(weight = 1f, fill = false),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    text = card.question
+                )
+                Checkbox(
+                    checked = card.isChecked,
+                    onCheckedChange = { onCardSelected(card.id) }
+                )
+            }
+
+            Divider(
+                modifier = Modifier
+                    .padding(vertical = 6.dp)
+            )
+            Row(
+                Modifier
+                    .height(IntrinsicSize.Min),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = "Tag: " + card.tag,
+                    fontSize = 12.sp,
+                )
+                Divider(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(0.5.dp),
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Text(
+                    modifier = Modifier,
+                    text = "Categories: " + card.categories.joinToString(separator = ", "),
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 12.sp
+                )
+            }
+            Row(
+                modifier = Modifier.height(4.dp)
+            ) {}
+        }
+    }
+    Row(
+        modifier = Modifier.height(4.dp)
+    ) {}
+}
