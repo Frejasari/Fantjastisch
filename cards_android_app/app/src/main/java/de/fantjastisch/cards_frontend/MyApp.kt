b8ef8a0d4d16e2dede0d76952261cec9f3b23731
Freja Sari <freja.sender@gmail.com>
2023-01-22 15:19:56 +0100

    ip...

--- cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/MyApp.kt
+++ cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/MyApp.kt
@@ -8,7 +8,7 @@ import de.fantjastisch.cards_frontend.config.Repository
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        System.setProperty("org.openapitools.client.baseUrl", "http://192.168.178.20:8080")
+        System.setProperty("org.openapitools.client.baseUrl", "http://10.0.2.2:8080")
         AppDatabase.database = Room.databaseBuilder(
             this,
             Repository::class.java, "database-name"
