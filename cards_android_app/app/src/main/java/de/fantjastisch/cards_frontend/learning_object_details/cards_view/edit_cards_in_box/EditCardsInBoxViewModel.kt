6f9bd0ad71bb2333a25790db766b7716d1c59b57
Semjon Nirmann <semjon.nirmann@yahoo.com>
2023-02-01 23:36:43 +0100

    refactor editCardsInBox... + add LoadingIcon to util

--- cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/learning_object_details/cards_view/edit_cards_in_box/EditCardsInBoxViewModel.kt
+++ cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/learning_object_details/cards_view/edit_cards_in_box/EditCardsInBoxViewModel.kt
@@ -3,30 +3,24 @@ package de.fantjastisch.cards_frontend.learning_object_details.cards_view.edit_c
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import de.fantjastisch.cards_frontend.card.CardRepository
 import de.fantjastisch.cards_frontend.card.CardSelectItem
-import de.fantjastisch.cards_frontend.glossary.CardsFilters
 import de.fantjastisch.cards_frontend.infrastructure.RepoResult
 import de.fantjastisch.cards_frontend.infrastructure.fold
-import de.fantjastisch.cards_frontend.learning_box.card_to_learning_box.CardToLearningBoxRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.openapitools.client.models.CardEntity
 import org.openapitools.client.models.ErrorEntryEntity
 import java.util.*
 
 class EditCardsInBoxViewModel(
     private val learningBoxId: UUID,
     private val learningObjectId: UUID,
-    private val cardRepository: CardRepository = CardRepository(),
-    private val cardToLearningBoxRepository: CardToLearningBoxRepository = CardToLearningBoxRepository()
+    private val model: EditCardsInBoxModel = EditCardsInBoxModel()
 ) : ViewModel() {
 
     val cards = mutableStateOf<List<CardSelectItem>>(mutableListOf())
     val errors = mutableStateOf<List<ErrorEntryEntity>>(emptyList())
     val error = mutableStateOf<String?>(null)
     val isFinished = mutableStateOf(false)
-
+    var isLoading = mutableStateOf(true)
 
     init {
         onPageLoaded()
@@ -34,56 +28,15 @@ class EditCardsInBoxViewModel(
 
     fun onPageLoaded() {
         viewModelScope.launch {
-            val result = cardRepository.getPage(
-                categoryIds = null,
-                search = null,
-                tag = null,
-                sort = null
-            )
-            when (result) {
-                is RepoResult.Success -> loadContainedCards(result.result)
-                is RepoResult.Error,
-                is RepoResult.ServerError -> error.value = "Ein Netzwerkfehler ist aufgetreten."
-            }
-        }
-    }
-
-    private fun loadContainedCards(allCards: List<CardEntity>) {
-        viewModelScope.launch {
-            val result = cardToLearningBoxRepository.getCardIdsForBox(learningBoxId = learningBoxId)
-
-            when (result) {
-                is RepoResult.Success -> {
-                    val listOfCardIdsInBox = result.result
-                    cardToLearningBoxRepository.getAllCardsForLearningObject(learningObjectId = learningObjectId)
-                        .fold(
-                            onSuccess = { listOfCardIdsInObject ->
-                                val cardsPresentInOtherBoxes =
-                                    listOfCardIdsInObject.filter { id ->
-                                        !listOfCardIdsInBox.contains(
-                                            id
-                                        )
-                                    }
-
-                                cards.value = allCards.filter { card ->
-                                    listOfCardIdsInBox.contains(card.id) || !cardsPresentInOtherBoxes.contains(
-                                        card.id
-                                    )
-                                }.map { card ->
-                                    CardSelectItem(
-                                        card = card,
-                                        isChecked = listOfCardIdsInBox.contains(card.id)
-                                    )
-                                }.sortedByDescending { it.isChecked }
-                            },
-                            onValidationError = {},
-                            onUnexpectedError = {})
-                }
-                is RepoResult.Error,
-                is RepoResult.ServerError -> {
-                    error.value = "Ein Netzwerkfehler ist aufgetreten."
-                }
-            }
+            model.initializePage(learningBoxId = learningBoxId, learningObjectId = learningObjectId)
+                .fold(
+                    onSuccess = {
+                        cards.value = it
+                        isLoading.value = false
+                    },
+                    onValidationError = { error.value = "Fehler bei der Eingabevalidierung." },
+                    onUnexpectedError = { error.value = "Ein unbekannter Fehler ist aufgetreten." },
+                )
         }
     }
 
@@ -104,10 +57,7 @@ class EditCardsInBoxViewModel(
                 .map { card -> card.card.id }
 
         viewModelScope.launch {
-            cardToLearningBoxRepository.updateBoxCards(
-                selected = selectedCardsIds,
-                learningBoxId = learningBoxId,
-            ).fold(
+            model.updateCardsInBox(selectedCardsIds=selectedCardsIds, learningBoxId=learningBoxId).fold(
                 onSuccess = { isFinished.value = true },
                 onValidationError = { error.value = "Fehler bei der Eingabevalidierung." },
                 onUnexpectedError = { error.value = "Ein unbekannter Fehler ist aufgetreten." }
