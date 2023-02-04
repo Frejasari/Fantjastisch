dea3bb37ac71f3963292ea9add08e78a37c19c58
Freja Sari <freja.sender@gmail.com>
2023-02-04 21:50:25 +0100

    add docs to app and change database name

--- cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/MyApp.kt
+++ cards_android_app/app/src/main/java/de/fantjastisch/cards_frontend/MyApp.kt
@@ -5,13 +5,18 @@ import androidx.room.Room
 import de.fantjastisch.cards_frontend.config.AppDatabase
 import de.fantjastisch.cards_frontend.config.Repository
 
+/**
+ * Applikationsklasse, die die Datenbank bereitstellt.
+ *
+ * @author Freja Sender
+ */
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         System.setProperty("org.openapitools.client.baseUrl", "http://10.0.2.2:8080")
         AppDatabase.database = Room.databaseBuilder(
             this,
-            Repository::class.java, "database-name"
+            Repository::class.java, "fantjastisch"
         ).build()
     }
 }
